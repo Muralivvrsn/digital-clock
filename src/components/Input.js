8bdc02868317033a58e09abe8bd7a585948007cf
Murali <muralivvrsn75683@gmail.com>
2023-01-08 07:32:22 +0530

    redux files deleted and input.js changed

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,19 @@
 
-import React, { useEffect, useState, useRef } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import React, {useState} from 'react'
 const Input = (props) => {
-  const hr = useSelector((state)=>state.hours);
-  const min = useSelector((state)=>state.minutes);
-  const sec = useSelector((state)=>state.seconds);
-  const INC = useSelector((state)=>state.INC);
-  console.log(hr,min,sec,INC)
-  const dispatch = useDispatch();
-  const update = ()=>props.func([hr,min,sec,true]);
+  const [Data , getData] = useState({hr:0,min:0,display:false});
+  const update = ()=>props.func(Data);
   return (
     <div className={(INC?"getAnimate":" ")+" classInput " + (props.display?' inputNoDisplay':' inputDisplay')}>
         <div className='input'>
-            <form action="">
-            <input type="number" min={0} max={24} onChange={(e)=>dispatch({type:'hours',playload:e.value})} autoFocus/>
+            <form action="" onSubmit={update}>
+            <input type="number" min={0} max={24} onChange={(e)=>getData({...Data, hr:e.target.value})} autoFocus/>
             hr
-            <input type="number" min={0} max={59}   onChange={(e)=>dispatch({type:'minutes',playload:e.value})}/>
+            <input type="number" min={0} max={59}   onChange={(e)=>getData({...Data, min:e.target.value})}/>
             min
-            <input type="number" min={0} max={59}  onChange={(e)=>dispatch({type:'seconds',playload:e.value})}/>
-            sec
+            <button id='input-btn'>set</button>
             </form>
         </div>
-        <button id='input-btn' onClick={update}>set</button>
     </div>
   )
 }
